9f78ce3065636a1ff2058262ac9da35594ee32ad
bothsupernova <90545201+bothsupernova@users.noreply.github.com>
2021-11-27 00:34:29 +0700

    rever

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdio.h>
 #define N 4
 
 void add(int A[][N], int B[][N], int C[][N])
@@ -8,6 +7,14 @@ void add(int A[][N], int B[][N], int C[][N])
 	for (i = 0; i < N; i++)
 		for (j = 0; j < N; j++)
 			C[i][j] = A[i][j] + B[i][j];
+
+	printf("Result matrix is \n");
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N; j++)
+		printf("%d ", C[i][j]);
+		printf("\n");
+	}
 }
 
 int main()
@@ -24,15 +31,9 @@ int main()
 
 	int C[N][N]; // To store result
 	int i, j;
-	add(A, B, C);
+	
 
-	printf("Result matrix is \n");
-	for (i = 0; i < N; i++)
-	{
-		for (j = 0; j < N; j++)
-		printf("%d ", C[i][j]);
-		printf("\n");
-	}
+	
 
-	return 0;
+
 }
